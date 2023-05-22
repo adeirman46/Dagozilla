b1b2e900fa305eaed3605d0d97a52a6f4b65d051
Adeirman46 <adeirman387@gmail.com>
2023-05-22 20:34:06 +0700

    Add client service

---
+++ Self_Learning/ROS/build/cpp_srvcli/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/irman/Documents/Programming/CPP/Dagozilla/Dagozilla/Self_Learning/ROS/src/cpp_srvcli
+# Build directory: /home/irman/Documents/Programming/CPP/Dagozilla/Dagozilla/Self_Learning/ROS/build/cpp_srvcli
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
